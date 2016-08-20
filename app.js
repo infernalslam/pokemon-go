b7a5bb6a6fa250c63344e636251849ac38d053bc
infernalslam <you@example.com>
2016-08-20 14:41:10 +0700

    get api

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-new Vue({
-    el: '#app',
-    data: {
-        msg: 'Hello Vue.js!'
+/*global Vue*/
+var app = new Vue({
+  el: '#app',
+  data: {
+    msg: 'Pokemon Ranger'
+  },
+  methods: {
+    getApi: function () {
+      var url = 'http://www.pokesnipers.com/api/v1/pokemon.json'
+      this.$http.get(url).then(function (res) {
+        console.log(res)
+      })
     }
+  }
 })
+console.log(app)
